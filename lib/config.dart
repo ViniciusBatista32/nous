ceb29a3ee63bda1657a2ed344e9a2ed658daa1d5
RaphaelCostaP <raphinhas_xd@hotmail.com>
2021-11-01 18:55:45 -0300

    splash screen e imagens

--- lib/config.dart
+++ lib/config.dart
@@ -1,8 +1,7 @@
-class Config
-{
+class Config {
   // Api key for database communication
   final String apiKey = "ZDMyMTliMDA1NTk3Mjg4Njk3YTE4NzkyMzQ2ZWU4OWQ=";
 
   // URL base for database request
-  final String baseUrl = "http://10.0.0.194/nous_api/";
+  final String baseUrl = "http://192.168.50.16/nous_api/";
 }
